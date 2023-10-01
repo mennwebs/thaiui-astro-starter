7d3ad7345b594707265bcdc7ea108691b29d5a3e
Menn <m@seedwebs.com>
2023-10-01 14:58:47 +0700

    Add prefetch & critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
+import critters from 'astro-critters';
 
 // https://astro.build/config
 export default defineConfig({
-
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    prefetch({
+      intentSelector: ["a[href^='/']"],
+    }),
+    critters(),
+  ],
 });
